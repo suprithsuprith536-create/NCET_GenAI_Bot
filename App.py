2cfd293a24c513f751854b68a87476a29d09fd04
suprithsuprith536-create <suprithsuprith536@gmail.com>
2026-04-15 10:44:00 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,3 +3,4 @@ from groq import Groq
 
 st.set_page_cofig("PragyanAI Content Generator", layout="wide")
 st.title("PragyanAI - Content Genrator")
+st.image("king-virat-kohli-hd-art-b2ulryvxkkh2bxla (2).jpg")
