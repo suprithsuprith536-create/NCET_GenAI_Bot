a8a621e19242ffd540b8a42ab14d7d2cde4dc8ee
suprithsuprith536-create <suprithsuprith536@gmail.com>
2026-04-15 11:38:16 +0530

    Update App.py

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ st.set_page_config("PragyanAI Content Generator", layout="wide")
 st.title("PragyanAI – Content Generator")
 st.image("PragyanAI_Transperent.jpg")
 # Get GROQ API Key
-client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+client = Groq(api_key=st.secrets["groq_api_key"])
 # Get Product Name and Audience for That Product
 product = st.text_input("Product")
 audience = st.text_input("Audience")
